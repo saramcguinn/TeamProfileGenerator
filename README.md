c6d3e55d6cdaa4167c0abde0bc5970a7932d86dc
sara.mcguinn <sara.mcguinn@gmail.com>
2020-09-12 22:48:19 -0500

    Added css styling, updated README

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # TeamProfileGenerator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+This is a software engineering team generator command line application. The application prompts the user for information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application creates an HTML file that displays a nicely formatted team roster based on the information provided by the user.
+
+Node:
+
+<img src="assets/node-screenshot.jpg" width=400>
+
+HTML in browser:
+
+<img src="assets/html-screenshot.jpg" width=400>
+
+For a brief demo video, see https://drive.google.com/file/d/1WLA1wegDDQY8ihx-IRQXdDrlwiXzzsTM/view.
+
+---
+
+## Installation
+This application requires Node.js and npm. It uses built-in Node modules `fs` and `path`, and it also requires installation of the `inquirer` module. To install `inquirer`, simply navigate into the directory where the index.js file is saved and type the following into the command line:
+
+```
+npm install inquirer --save
+```
+To run tests, the application will also require installation of the node module `jest`.
+
+---
+
+## Usage
+After inquirer is installed, you're ready to go. To run the program in Node, type the following into the command line:
+
+```
+node app.js
+```
+
+Then type in your answers to the prompts. Once all questions are answered, a team.html file will be generated, populated by your answers. Open the html file in the browser to see all your team members neatly displayed.
+
+---
+
+## Tests 
+This application includes files for tests, which are run using `jest`. After installing jest, you can run tests at any time by typing the following into the command line:
+
+```
+npm run test
+```
+
+---
+
+## Questions
+Please feel free to contact me with any questions about this project.
+
+Name: Sara McGuinn
+
+GitHub: saramcguinn
+
+Email: sara.s.mcguinn@gmail.com
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Require files and modules & declare variables
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -12,8 +13,7 @@ const render = require("./lib/htmlRenderer");
 
 let employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Function uses inquirer to gather information about team members and creates objects for each team member
 function getEmployeeInfo() {
     inquirer.prompt([
         {
@@ -75,6 +75,7 @@ function getEmployeeInfo() {
     });
 }
 
+// Function allows user to add additional employees, and once finished, creates html file
 function addEmployee() {
     inquirer.prompt([
         {
@@ -86,37 +87,17 @@ function addEmployee() {
     ])
     .then((response) => {
         if (response.addEmployee === "Yes") {
+            console.log("-------------------")
             getEmployeeInfo();
         } else {
             let html = render(employees);
             fs.writeFile(outputPath, html, (err) => {
                 if (err) throw err;
             });
+            console.log("File 'team.html' successfully generated and saved in 'output' directory.")
         }
     });
 }
 
-
+// Call function to begin application
 getEmployeeInfo();
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
